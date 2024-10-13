74a7bcc226289ae29d609c5f0e6bb9c12766c952
sameer-ahmed-2000 <sameersameersmr20@gmail.com>
2024-10-13 15:29:11 +0530

    added logout

--- client/src/components/navBar.tsx
+++ client/src/components/navBar.tsx
@@ -2,12 +2,13 @@ import { useDispatch, useSelector } from "react-redux";
 import { connectWallet, disconnectWallet, setWalletError } from "../store/slices/walletAddressSlice";
 import { ethers } from "ethers";
 import { RootState } from "../store/store"
+import { useAuth } from "../auth/authContext";
 
 export const NavBar = () => {
     const dispatch = useDispatch();
     const state=useSelector((state:RootState)=>state)
     const walletAddress = state.walletAddress.walletAddress
-
+    const {logout}=useAuth()
     const handleConnect = async () => {
         if (typeof window !== "undefined" && window.ethereum) {
             try {
@@ -34,14 +35,15 @@ export const NavBar = () => {
     };
 
     return (
-        <nav className="px-8 bg-[#3074f4] h-12 flex justify-end items-center">
+        <nav className="px-4 bg-[#3074f4] h-12 flex justify-end items-center">
             {walletAddress ? (
                 <p>
-                    {walletAddress} <button className="bg-[#FFFFFF] rounded-md h-8 w-24 font-bold" onClick={handleDisconnect}>Disconnect</button>
+                    {walletAddress} <button className="bg-[#FFFFFF] rounded-md h-8 w-24 mx-1 font-bold" onClick={handleDisconnect}>Disconnect</button>
                 </p>
             ) : (
-                <button className="bg-[#FFFFFF] rounded-md h-8 w-24 font-bold" onClick={handleConnect}>Connect</button>
+                <button className="bg-[#FFFFFF] rounded-md h-8 w-24 font-bold mx-1" onClick={handleConnect}>Connect</button>
             )}
+            <button className="bg-[#FFFFFF] rounded-md h-8 w-24 font-bold mx-1" onClick={logout}>Log out</button>
         </nav>
     )
 }
