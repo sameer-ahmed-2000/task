e70aa61e06eafc36c30555b376275226ca7c19ad
sameer-ahmed-2000 <sameersameersmr20@gmail.com>
2024-10-13 23:37:03 +0530

    updated route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/signup" element={<Signup />} />
             <Route path="/signin" element={<Signin />} />
+            <Route path='*' element={<Signin/>}/>
             <Route
               path="/dashboard"
               element={
